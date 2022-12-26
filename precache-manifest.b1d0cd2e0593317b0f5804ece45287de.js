955e3f87a8c5def3eb6b8a1677c33c7c9a6cc440
blagoje.vukovic93 <blagoje.vukovic93@gmail.com>
2022-12-26 16:22:32 +0100

    Updates

---
+++ precache-manifest.b1d0cd2e0593317b0f5804ece45287de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbb0217ae9b07a026d59ac9b6a80231",
+    "url": "/SongPop-Playlist-Randomizer-Web/index.html"
+  },
+  {
+    "revision": "8a6a5b970ea0489abf66",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/css/2.0b144708.chunk.css"
+  },
+  {
+    "revision": "847e7d2aefa6d2e06ed5",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/css/main.f745b893.chunk.css"
+  },
+  {
+    "revision": "8a6a5b970ea0489abf66",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/js/2.30c374a0.chunk.js"
+  },
+  {
+    "revision": "e7c69aa4549a0938e155d41214f1973f",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/js/2.30c374a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847e7d2aefa6d2e06ed5",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/js/main.16a5b905.chunk.js"
+  },
+  {
+    "revision": "4f97ab4b3d7e20196363",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/js/runtime-main.4115bb67.js"
+  },
+  {
+    "revision": "ce6e65456cb9b141639943bcb576aed4",
+    "url": "/SongPop-Playlist-Randomizer-Web/static/media/sppic.ce6e6545.png"
+  }
+]);
